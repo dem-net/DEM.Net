3743ed114e9db3b58593e381cf93d2aa9b9e2c63
Xavier Fischer <xavier@fischer.ninja>
2019-04-06 23:39:09 +0200

    STL export service

--- SampleApp/Samples/STLSamples.cs
+++ SampleApp/Samples/STLSamples.cs
@@ -1,8 +1,8 @@
 ﻿using AssetGenerator;
 using AssetGenerator.Runtime;
 using DEM.Net.glTF;
+using DEM.Net.glTF.Export;
 using DEM.Net.Lib;
-using IxMilia.Stl;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -39,58 +39,14 @@ namespace SampleApp
             mesh.RotateX((float)Math.PI / 2f);
 
             var stlFileName = Path.Combine(outputDirectory, $"{modelName}.stl");
-
-            STLExport(mesh, stlFileName, false);
+            STLExportService stlService = new STLExportService();
+            stlService.STLExport(mesh, stlFileName, false);
 
             Model model = glTFService.GenerateModel(mesh, modelName);
             glTFService.Export(model, outputDirectory, $"{modelName}", false, true);
 
         }
 
-        public static void STLExport(MeshPrimitive mesh, string fileName, bool ascii = true)
-        {
-            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
-            // ...
-
-            StlFile stlFile = new StlFile();
-            stlFile.SolidName = "dem-net";
-
-            //The number of the vertices
-            var positions = mesh.Positions.ToList();
-            var indices = mesh.Indices.ToList();
-
-            stlFile.Triangles.Capacity = indices.Count / 3;
-            int numTriangle = 0;
-            for (int i = 0; i < indices.Count; i += 3)
-            {
-
-                stlFile.Triangles.Add(CreateStlTriangle(positions[indices[i]]
-                                            , positions[indices[i + 1]]
-                                            , positions[indices[i + 2]]));
-                numTriangle++;
-            }
-
-            // ...
-
-            using (FileStream fs = new FileStream(fileName, FileMode.Create))
-            {
-                stlFile.Save(fs, ascii);
-            }
-        }
-
-        private static StlTriangle CreateStlTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
-        {
-            //Compute the triangle's normal
-            Vector3 dir = Vector3.Normalize(Vector3.Cross(v2 - v1, v3 - v1));
-            return new StlTriangle(CreateStlNormal(dir), CreateStlVertex(v1), CreateStlVertex(v2), CreateStlVertex(v3));
-        }
-        private static StlNormal CreateStlNormal(Vector3 normal)
-        {
-            return new StlNormal(normal.X, normal.Y, normal.Z);
-        }
-        private static StlVertex CreateStlVertex(Vector3 vertex)
-        {
-            return new StlVertex(vertex.X, vertex.Y, vertex.Z);
-        }
+        
     }
 }
