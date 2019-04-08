ba046cf9f1112b1b8ba823746fde977a178da555
Xavier Fischer <xavier@fischer.ninja>
2019-04-08 19:56:57 +0200

    Samples

--- DEM.Net.Samples/Program.cs
+++ DEM.Net.Samples/Program.cs
@@ -6,13 +6,23 @@ namespace DEM.Net.Samples
 {
     class Program
     {
-        static string _OutputDataDirectory = @"..\..\..\Data";
+        static string _OutputDataDirectory = @"..\..\..\..\Data";
 
         static void Main(string[] args)
         {
             Logger.StartPerf("Main cold start");
 
             //DatasetSamples.Run();
+
+
+            //string bboxTest = "POLYGON ((5.558267 43.538602, 5.557902 43.538602, 5.557902 43.538353, 5.558267 43.538353, 5.558267 43.538602))";
+            //STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), "Test", bboxTest, DEMDataSet.AW3D30);
+
+            STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), DEMDataSet.AW3D30);
+
+            GpxSamples gpxSamples = new GpxSamples(_OutputDataDirectory, Path.Combine(_OutputDataDirectory, @"GPX\venturiers.gpx"));
+            gpxSamples.Run();
+
 
             //ElevationSamples.Run();
 
@@ -25,18 +35,6 @@ namespace DEM.Net.Samples
             reprojSamples.Run();
 
 
-
-            string bboxTest = "POLYGON ((5.558267 43.538602, 5.557902 43.538602, 5.557902 43.538353, 5.558267 43.538353, 5.558267 43.538602))";
-            STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), "Test", bboxTest, DEMDataSet.AW3D30);
-
-            string bboxMtBlanc = "POLYGON ((6.944733 45.904438, 6.778999 45.904438, 6.778999 45.776144, 6.944733 45.776144, 6.944733 45.904438))";
-            STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), "Mont Blanc", bboxMtBlanc, DEMDataSet.AW3D30);
-            string steVictoire = "POLYGON((5.54888 43.519525, 5.61209 43.519525, 5.61209 43.565225, 5.54888 43.565225, 5.54888 43.519525))";
-            STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), "Ste Victoire", steVictoire, DEMDataSet.AW3D30);
-            string bboxSantiagoChile = "POLYGON ((-69.647827 -33.767732, -69.647827 -32.953368, -70.751202 -32.953368, -70.751202 -33.767732, -69.647827 -33.767732))";
-            STLSamples.Run(Path.Combine(_OutputDataDirectory, "glTF"), "Santiago de Chile", bboxSantiagoChile, DEMDataSet.SRTM_GL3);
-            //GpxSamples gpxSamples = new GpxSamples(_OutputDataDirectory, @"..\..\..\Data\GPX\venturiers.gpx");
-            //gpxSamples.Run();
 
 
 
--- DEM.Net.Samples/Samples/STLSamples.cs
+++ DEM.Net.Samples/Samples/STLSamples.cs
@@ -16,9 +16,15 @@ namespace DEM.Net.Samples
 {
     class STLSamples
     {
-        public static void Run(string outputDirectory, string modelName, string bboxWKT, DEMDataSet dataset)
+        public static void Run(string outputDirectory, DEMDataSet dataset)
         {
-           
+            string modelName= "Montagne Sainte Victoire";
+            string bboxWKT = "POLYGON((5.54888 43.519525, 5.61209 43.519525, 5.61209 43.565225, 5.54888 43.565225, 5.54888 43.519525))";
+            //string modelName = "Canigou";
+            //string bboxWKT = "POLYGON ((2.523766 42.557131, 2.377167 42.557131, 2.377167 42.495137, 2.523766 42.495137, 2.523766 42.557131))";
+            //string modelName = "Mont Blanc";
+            //string bboxWKT = "POLYGON ((6.944733 45.904438, 6.778999 45.904438, 6.778999 45.776144, 6.944733 45.776144, 6.944733 45.904438))";
+
             // small test
             //string bboxWKT = "POLYGON ((5.558267 43.538602, 5.557902 43.538602, 5.557902 43.538353, 5.558267 43.538353, 5.558267 43.538602))";// zoom ste
             RasterService rasterService = new RasterService();
@@ -26,8 +32,8 @@ namespace DEM.Net.Samples
 
             var bbox = GeometryService.GetBoundingBox(bboxWKT);
             //bbox = bbox.Scale(1.3); // test
-            var heightMap = elevationService.GetHeightMap(bbox, dataset);
-            heightMap = heightMap.ReprojectGeodeticToCartesian()
+            var heightMap = elevationService.GetHeightMap(bbox, dataset)
+                                    .ReprojectGeodeticToCartesian()
                                     .ZScale(2f)
                                     .CenterOnOrigin()
                                     .FitInto(250f)
@@ -47,6 +53,6 @@ namespace DEM.Net.Samples
 
         }
 
-        
+
     }
 }
