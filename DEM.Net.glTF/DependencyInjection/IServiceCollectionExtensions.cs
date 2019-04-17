b57d0c92980e1b65f4ced90f2734d264a97b5bd4
Xavier Fischer <xavier@fischer.ninja>
2019-04-17 23:46:28 +0200

    dependency injection and logging with ILogger

---
+++ DEM.Net.glTF/DependencyInjection/IServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DEM.Net.Core.Imagery;
+using DEM.Net.Core.Services.Mesh;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DEM.Net.glTF
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddDemNetglTF(this IServiceCollection services)
+        {
+            services
+                    .AddTransient<IMeshService, MeshService>()
+                    .AddTransient<IglTFService, glTFService>();
+
+            return services;
+        }
+    }
+}
