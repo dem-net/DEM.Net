f7c80076a35e30dad90775e6f8bece590d2c5766
Xavier Fischer <xavier@fischer.ninja>
2019-11-01 00:44:35 +0100

    tiled geotiff ok

--- DEM.Net.Core/IO/Raster/NoLogTiffErrorHandler.cs
+++ DEM.Net.Core/IO/Raster/NoLogTiffErrorHandler.cs
@@ -24,6 +24,7 @@
 // THE SOFTWARE.
 
 using BitMiracle.LibTiff.Classic;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -52,26 +53,26 @@ namespace DEM.Net.Core.IO
 			//base.WarningHandlerExt(tif, clientData, method, format, args);
 		}
 	}
-    internal class ConsoleLogTiffErrorHandler : TiffErrorHandler
+    internal class TraceTiffErrorHandler : TiffErrorHandler
     {
         public override void ErrorHandler(Tiff tif, string method, string format, params object[] args)
         {
-            Debug.WriteLine(method + " " + string.Format(format,args));
+            Trace.TraceError(method + " " + string.Format(format,args));
         }
         public override void ErrorHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
         {
 
-            Debug.WriteLine(method + " " + string.Format(format, args));
+            //Debug.WriteLine(method + " " + string.Format(format, args));
         }
         public override void WarningHandler(Tiff tif, string method, string format, params object[] args)
         {
 
-            Debug.WriteLine(method + " " + string.Format(format, args));
+            //Debug.WriteLine(method + " " + string.Format(format, args));
         }
         public override void WarningHandlerExt(Tiff tif, object clientData, string method, string format, params object[] args)
         {
 
-            Debug.WriteLine(method + " " + string.Format(format, args));
+           // Debug.WriteLine(method + " " + string.Format(format, args));
         }
     }
 }
