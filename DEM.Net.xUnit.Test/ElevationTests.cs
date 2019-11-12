503cc5061eaba0398dda41fcbb61030d04be59ee
xfischer <xavier@fischer.ninja>
2019-11-12 18:47:51 +0100

    secrets

--- DEM.Net.xUnit.Test/ElevationTests.cs
+++ DEM.Net.xUnit.Test/ElevationTests.cs
@@ -83,7 +83,7 @@ namespace DEM.Net.Test
         }
 
         [Theory()]
-        [InlineData(nameof(DEMDataSet.SRTM_GL3), -25, -26, 37, 38, true)] // fully covered
+        [InlineData(nameof(DEMDataSet.SRTM_GL3), -26, -25, 37, 38, true)] // fully covered
         [InlineData(nameof(DEMDataSet.SRTM_GL3), -26.659806263787523, -25.729350373606543, 37.73596920859053, 38.39764411353181, false)] // 1 tile missing
         [InlineData(nameof(DEMDataSet.SRTM_GL3), -37.43596931765545, -37.13861749268079, 50.33844888725473, 50.51342652633956, false)] // not covered at all
         [InlineData(nameof(DEMDataSet.SRTM_GL3), 1.5, 2.5, 44.5, 45.5, true)] // fully covered by 4 tiles
