294fdfc3f8f40b5ba0ce34dea4f1d7b10df8f839
Xavier Fischer <xavier@fischer.ninja>
2019-12-10 23:23:40 +0100

    sensorlog parsing

---
+++ DEM.Net.Core/IO/SensorLog/SensorLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DEM.Net.Core.IO.SensorLog
+{
+    public static class SensorLog
+    {
+        public static List<SensorLogData> FromJson(string filePath)
+        {
+            return SensorLogData.FromJson(File.ReadAllText(filePath, Encoding.UTF8));
+        }
+
+    }
+}
