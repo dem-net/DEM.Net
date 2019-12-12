0221287fec5ebbd1180a3a1bc71e3c0c7fc53040
Xavier Fischer <xavier@fischer.ninja>
2019-12-12 17:50:13 +0100

    sensorlog model

--- DEM.Net.Core/IO/SensorLog/SensorLog.cs
+++ DEM.Net.Core/IO/SensorLog/SensorLog.cs
@@ -6,11 +6,31 @@ using System.Text;
 
 namespace DEM.Net.Core.IO.SensorLog
 {
-    public static class SensorLog
+    public class SensorLog
     {
-        public static List<SensorLogData> FromJson(string filePath)
+        public List<SensorLogData> Data { get; private set; }
+        public int LocationCount { get; private set; }
+        public int Count { get; private set; }
+        public int AltitudeCount { get; private set; }
+        public float AltitudeMin { get; private set; }
+        public float AltitudeMax { get; private set; }
+
+        public static SensorLog FromJson(string filePath)
         {
-            return SensorLogData.FromJson(File.ReadAllText(filePath, Encoding.UTF8));
+            SensorLog sensorLog = new SensorLog();
+
+            sensorLog.Data = SensorLogData.FromJson(File.ReadAllText(filePath, Encoding.UTF8));
+
+            sensorLog.Count = sensorLog.Data.Count;
+            foreach(var data in sensorLog.Data)
+            {
+                sensorLog.LocationCount += (data.LocationLongitude != 0 && data.LocationLatitude != 0) ? 1 : 0;
+                sensorLog.AltitudeCount += (data.LocationAltitude != 0) ? 1 : 0;
+            }
+            sensorLog.AltitudeMin = sensorLog.Data.Min(s => s.LocationAltitude);
+            sensorLog.AltitudeMax = sensorLog.Data.Max(s => s.LocationAltitude);
+
+            return sensorLog;
         }
 
     }
