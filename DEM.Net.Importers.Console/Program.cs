1e3d39b50fc1d7c24c96574dc92b0230dd3feb12
Xavier Fischer <xavier@fischer.ninja>
2020-01-20 23:51:23 +0100

    initial commit

---
+++ DEM.Net.Importers.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using DEM.Net.Importers.netCDF;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEM.Net.Importers.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Sample sample = new Sample();
+            sample.Run(null);
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ DEM.Net.Importers.netCDF/Sample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Research.Science.Data;
+using Microsoft.Research.Science.Data.NetCDF4;
+using Microsoft.Extensions.Logging;
+
+namespace DEM.Net.Importers.netCDF
+{
+    public class Sample
+    {
+
+        public void Run(ILogger logger)
+        {
+            try
+            {
+                string dataFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "gebco_2019_ajaccio.nc");
+
+                using (var ds = DataSet.Open(dataFile, ResourceOpenMode.ReadOnly))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("Metadata:");
+                    foreach (var data in ds.Metadata)
+                    {
+                        sb.AppendLine($"{data.Key}: {data.Value}");
+                    }
+                    sb.AppendLine("Variables:");
+                    foreach (var v in ds.Variables)
+                    {
+                        sb.AppendLine(v.ToString());
+                    }
+
+
+                    
+                    //logger.LogDebug(sb.ToString());
+                    Console.WriteLine(sb.ToString());
+                    //ds.Metadata[vname] = data;
+                }
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
+
