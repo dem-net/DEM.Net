9a9d6c8fb809c1c808435033e5dadfec3969d55f
Xavier Fischer <xavier@fischer.ninja>
2020-01-23 00:37:21 +0100

    fix typo

--- DEM.Net.Core/Model/Datasets/DEMDataSet.cs
+++ DEM.Net.Core/Model/Datasets/DEMDataSet.cs
@@ -62,89 +62,89 @@ namespace DEM.Net.Core
         private static Dictionary<string, DEMDataSet> GetRegisteredDatasets()
         {
             Dictionary<string, DEMDataSet> datasets = new Dictionary<string, DEMDataSet>();
-            //datasets.Add("SRTM_GL3", new DEMDataSet()
-            //{
-            //    Name = "SRTM_GL3",
-            //    Description = "Shuttle Radar Topography Mission (SRTM GL3) Global 90m",
-            //    PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTSRTM.042013.4326.1",
-            //    DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/SRTM_GL3/SRTM_GL3_srtm.vrt"),
-            //    FileFormat = new DEMFileDefinition("Nasa SRTM HGT", DEMFileType.SRTM_HGT, ".hgt", DEMFileRegistrationMode.Grid),
-            //    ResolutionMeters = 90,
-            //    ResolutionArcSeconds = 3 
-            //    PointsPerDegree = 1200,
-            //    Attribution = new Attribution(ATTRIBUTION_SUBJECT, "SRTM_GL3 OpenTopography", "https://opentopography.org/"
-            //        , @"Citing LP DAVV and Data Products: https://lpdaac.usgs.gov/about/citing_lp_daac_and_data
-            //            If you wish to cite the SRTM products in a report or publication please use: 
-            //            Farr, T. G., and M. Kobrick, 2000, Shuttle Radar Topography Mission produces a wealth of data. Eos Trans. AGU, 81:583-583.
-            //            Farr, T. G. et al., 2007, The Shuttle Radar Topography Mission, Rev. Geophys., 45, RG2004, doi:10.1029/2005RG000183. (Also available online at http://www2.jpl.nasa.gov/srtm/SRTM_paper.pdf)
-            //            Kobrick, M., 2006, On the toes of giants--How SRTM was born, Photogramm. Eng. Remote Sens., 72:206-210.
-            //            Rosen, P. A. et al., 2000, Synthetic aperture radar interferometry, Proc. IEEE, 88:333-382.
-            //            https://doi.org/10.5069/G9445JDF")
-            //});
-            //datasets.Add("SRTM_GL1", new DEMDataSet()
-            //{
-            //    Name = "SRTM_GL1",
-            //    Description = "Shuttle Radar Topography Mission (SRTM GL1) Global 30m",
-            //    PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTSRTM.082015.4326.1",
-            //    DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/SRTM_GL1/SRTM_GL1_srtm.vrt"),
-            //    FileFormat = new DEMFileDefinition("Nasa SRTM HGT", DEMFileType.SRTM_HGT, ".hgt", DEMFileRegistrationMode.Grid),
-            //    ResolutionMeters = 30,
-            //    ResolutionArcSeconds = 1,
-            //    PointsPerDegree = 3600,
-            //    Attribution = new Attribution(ATTRIBUTION_SUBJECT, "SRTM_GL1 OpenTopography", "https://opentopography.org/", "http://www2.jpl.nasa.gov/srtm/srtmBibliography.html, https://doi.org/10.5069/G9445JDF")
-            //}); ;
-            //datasets.Add("AW3D30", new DEMDataSet()
-            //{
-            //    Name = "AW3D30",
-            //    Description = "ALOS World 3D - 30m (nicest but contain void areas)",
-            //    PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTALOS.112016.4326.2",
-            //    DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/AW3D30/AW3D30_alos.vrt"),
-            //    FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Cell),
-            //    ResolutionMeters = 30,
-            //    ResolutionArcSeconds = 1,
-            //    PointsPerDegree = 3600,
-            //    NoDataValue = -9999,
-            //    Attribution = new Attribution(ATTRIBUTION_SUBJECT, "AW3D30 OpenTopography", "https://opentopography.org/"
-            //        , @"J. Takaku, T. Tadono, K. Tsutsui : Generation of High Resolution Global DSM from ALOS PRISM, The International Archives of the Photogrammetry, Remote Sensing and Spatial Information Sciences, pp.243-248, Vol. XL-4, ISPRS TC IV Symposium, Suzhou, China, 2014. [http://www.int-arch-photogramm-remote-sens-spatial-inf-sci.net/XL-4/243/2014/isprsarchives-XL-4-243-2014.pdf]
-            //        T.Tadono, H.Ishida, F.Oda, S.Naito, K.Minakawa, H.Iwamoto : Precise Global DEM Generation By ALOS PRISM, ISPRS Annals of the Photogrammetry, Remote Sensing and Spatial Information Sciences, pp.71 - 76, Vol.II - 4, 2014. [http://www.isprs-ann-photogramm-remote-sens-spatial-inf-sci.net/II-4/71/2014/isprsannals-II-4-71-2014.pdf]
-            //        https://doi.org/10.5069/G94M92HB
-            //        ")
-            //});
-            //datasets.Add("ETOPO1", new DEMDataSet()
-            //{
-            //    Name = "ETOPO1",
-            //    Description = "Global low res coverage with bathymetry (1km resolution)",
-            //    PublicUrl = "https://www.ngdc.noaa.gov/mgg/global/",
-            //    DataSource = new LocalFileSystem(localDirectory: Path.Combine("Data", "ETOPO1")),
-            //    FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Grid),
-            //    ResolutionMeters = 1800,
-            //    ResolutionArcSeconds = 60,
-            //    PointsPerDegree = 60,
-            //    NoDataValue = -9999,
-            //    Attribution = new Attribution(ATTRIBUTION_SUBJECT, "ETOPO1 - NOAA", "https://www.ngdc.noaa.gov/mgg/global/"
-            //        , "Amante, C. and B.W. Eakins, 2009. ETOPO1 1 Arc-Minute Global Relief Model: Procedures, Data Sources and Analysis. NOAA Technical Memorandum NESDIS NGDC-24. National Geophysical Data Center, NOAA. doi:10.7289/V5C8276M")
-            //});
-            //datasets.Add("ASTER_GDEMV3", new DEMDataSet()
-            //{
-            //    Name = "ASTER_GDEMV3",
-            //    Description = "ASTER Global Digital Elevation Model 1 arc second (30m)",
-            //    PublicUrl = "https://lpdaac.usgs.gov/products/astgtmv003",
-            //    DataSource = new NasaGranuleDataSource(indexFilePath: "ASTGTM.003.json", collectionId: "C1575726572-LPDAAC_ECS"),
-            //    FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Grid),
-            //    ResolutionMeters = 30,
-            //    ResolutionArcSeconds = 1,
-            //    PointsPerDegree = 3600,
-            //    NoDataValue = -9999,
-            //    Attribution = new Attribution(ATTRIBUTION_SUBJECT, "ASTER_GDEMV3",
-            //                                    "https://doi.org/10.5067/ASTER/ASTGTM.003",
-            //                                    "NASA/METI/AIST/Japan Spacesystems, and U.S./Japan ASTER Science Team. ASTER Global Digital Elevation Model V003. 2018, distributed by NASA EOSDIS Land Processes DAAC")
-            //});
+            datasets.Add("SRTM_GL3", new DEMDataSet()
+            {
+                Name = "SRTM_GL3",
+                Description = "Shuttle Radar Topography Mission (SRTM GL3) Global 90m",
+                PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTSRTM.042013.4326.1",
+                DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/SRTM_GL3/SRTM_GL3_srtm.vrt"),
+                FileFormat = new DEMFileDefinition("Nasa SRTM HGT", DEMFileType.SRTM_HGT, ".hgt", DEMFileRegistrationMode.Grid),
+                ResolutionMeters = 90,
+                ResolutionArcSeconds = 3,
+                PointsPerDegree = 1200,
+                Attribution = new Attribution(ATTRIBUTION_SUBJECT, "SRTM_GL3 OpenTopography", "https://opentopography.org/"
+                    , @"Citing LP DAVV and Data Products: https://lpdaac.usgs.gov/about/citing_lp_daac_and_data
+                        If you wish to cite the SRTM products in a report or publication please use: 
+                        Farr, T. G., and M. Kobrick, 2000, Shuttle Radar Topography Mission produces a wealth of data. Eos Trans. AGU, 81:583-583.
+                        Farr, T. G. et al., 2007, The Shuttle Radar Topography Mission, Rev. Geophys., 45, RG2004, doi:10.1029/2005RG000183. (Also available online at http://www2.jpl.nasa.gov/srtm/SRTM_paper.pdf)
+                        Kobrick, M., 2006, On the toes of giants--How SRTM was born, Photogramm. Eng. Remote Sens., 72:206-210.
+                        Rosen, P. A. et al., 2000, Synthetic aperture radar interferometry, Proc. IEEE, 88:333-382.
+                        https://doi.org/10.5069/G9445JDF")
+            });
+            datasets.Add("SRTM_GL1", new DEMDataSet()
+            {
+                Name = "SRTM_GL1",
+                Description = "Shuttle Radar Topography Mission (SRTM GL1) Global 30m",
+                PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTSRTM.082015.4326.1",
+                DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/SRTM_GL1/SRTM_GL1_srtm.vrt"),
+                FileFormat = new DEMFileDefinition("Nasa SRTM HGT", DEMFileType.SRTM_HGT, ".hgt", DEMFileRegistrationMode.Grid),
+                ResolutionMeters = 30,
+                ResolutionArcSeconds = 1,
+                PointsPerDegree = 3600,
+                Attribution = new Attribution(ATTRIBUTION_SUBJECT, "SRTM_GL1 OpenTopography", "https://opentopography.org/", "http://www2.jpl.nasa.gov/srtm/srtmBibliography.html, https://doi.org/10.5069/G9445JDF")
+            }); ;
+            datasets.Add("AW3D30", new DEMDataSet()
+            {
+                Name = "AW3D30",
+                Description = "ALOS World 3D - 30m (nicest but contain void areas)",
+                PublicUrl = "http://opentopo.sdsc.edu/raster?opentopoID=OTALOS.112016.4326.2",
+                DataSource = new VRTDataSource("https://cloud.sdsc.edu/v1/AUTH_opentopography/Raster/AW3D30/AW3D30_alos.vrt"),
+                FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Cell),
+                ResolutionMeters = 30,
+                ResolutionArcSeconds = 1,
+                PointsPerDegree = 3600,
+                NoDataValue = -9999,
+                Attribution = new Attribution(ATTRIBUTION_SUBJECT, "AW3D30 OpenTopography", "https://opentopography.org/"
+                    , @"J. Takaku, T. Tadono, K. Tsutsui : Generation of High Resolution Global DSM from ALOS PRISM, The International Archives of the Photogrammetry, Remote Sensing and Spatial Information Sciences, pp.243-248, Vol. XL-4, ISPRS TC IV Symposium, Suzhou, China, 2014. [http://www.int-arch-photogramm-remote-sens-spatial-inf-sci.net/XL-4/243/2014/isprsarchives-XL-4-243-2014.pdf]
+                    T.Tadono, H.Ishida, F.Oda, S.Naito, K.Minakawa, H.Iwamoto : Precise Global DEM Generation By ALOS PRISM, ISPRS Annals of the Photogrammetry, Remote Sensing and Spatial Information Sciences, pp.71 - 76, Vol.II - 4, 2014. [http://www.isprs-ann-photogramm-remote-sens-spatial-inf-sci.net/II-4/71/2014/isprsannals-II-4-71-2014.pdf]
+                    https://doi.org/10.5069/G94M92HB
+                    ")
+            });
+            datasets.Add("ETOPO1", new DEMDataSet()
+            {
+                Name = "ETOPO1",
+                Description = "Global low res coverage with bathymetry (1km resolution)",
+                PublicUrl = "https://www.ngdc.noaa.gov/mgg/global/",
+                DataSource = new LocalFileSystem(localDirectory: Path.Combine("Data", "ETOPO1")),
+                FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Grid),
+                ResolutionMeters = 1800,
+                ResolutionArcSeconds = 60,
+                PointsPerDegree = 60,
+                NoDataValue = -9999,
+                Attribution = new Attribution(ATTRIBUTION_SUBJECT, "ETOPO1 - NOAA", "https://www.ngdc.noaa.gov/mgg/global/"
+                    , "Amante, C. and B.W. Eakins, 2009. ETOPO1 1 Arc-Minute Global Relief Model: Procedures, Data Sources and Analysis. NOAA Technical Memorandum NESDIS NGDC-24. National Geophysical Data Center, NOAA. doi:10.7289/V5C8276M")
+            });
+            datasets.Add("ASTER_GDEMV3", new DEMDataSet()
+            {
+                Name = "ASTER_GDEMV3",
+                Description = "ASTER Global Digital Elevation Model 1 arc second (30m)",
+                PublicUrl = "https://lpdaac.usgs.gov/products/astgtmv003",
+                DataSource = new NasaGranuleDataSource(indexFilePath: "ASTGTM.003.json", collectionId: "C1575726572-LPDAAC_ECS"),
+                FileFormat = new DEMFileDefinition("GeoTiff file", DEMFileType.GEOTIFF, ".tif", DEMFileRegistrationMode.Grid),
+                ResolutionMeters = 30,
+                ResolutionArcSeconds = 1,
+                PointsPerDegree = 3600,
+                NoDataValue = -9999,
+                Attribution = new Attribution(ATTRIBUTION_SUBJECT, "ASTER_GDEMV3",
+                                                "https://doi.org/10.5067/ASTER/ASTGTM.003",
+                                                "NASA/METI/AIST/Japan Spacesystems, and U.S./Japan ASTER Science Team. ASTER Global Digital Elevation Model V003. 2018, distributed by NASA EOSDIS Land Processes DAAC")
+            });
             datasets.Add("GEBCO_2019", new DEMDataSet()
             {
                 Name = "GEBCO_2019",
                 Description = "GEBCO’s gridded bathymetric data set, a global terrain model for ocean and land at 15 arc-second intervals",
                 PublicUrl = "https://www.gebco.net/data_and_products/gridded_bathymetry_data/gebco_2019/gebco_2019_info.html",
-                DataSource = new LocalFileSystem(localDirectory: Path.Combine("Data", "GEBC_2019")),
+                DataSource = new LocalFileSystem(localDirectory: Path.Combine("Data", "GEBCO_2019")),
                 FileFormat = new DEMFileDefinition("netCDF file", DEMFileType.CF_NetCDF, ".nc", DEMFileRegistrationMode.Cell),
                 ResolutionMeters = 463,
                 ResolutionArcSeconds = 15,
