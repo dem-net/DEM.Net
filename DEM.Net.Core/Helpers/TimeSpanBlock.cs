0153764a4b4666a662dc455602c16d30f361ddc6
xfischer <xavier@fischer.ninja>
2020-02-05 13:36:54 +0100

    TimeSpanblock accepts log level

--- DEM.Net.Core/Helpers/TimeSpanBlock.cs
+++ DEM.Net.Core/Helpers/TimeSpanBlock.cs
@@ -31,21 +31,26 @@ namespace DEM.Net.Core
 {
     public class TimeSpanBlock : IDisposable
     {
-        private  Stopwatch _sw;
-        private  ILogger _logger;
+        private Stopwatch _sw;
+        private ILogger _logger;
         private readonly string _operationName;
+        private readonly LogLevel _logLevel;
 
-        public TimeSpanBlock(string operationName, ILogger logger)
+        public TimeSpanBlock(string operationName, ILogger logger, LogLevel logLevel = LogLevel.Information)
         {
             _sw = Stopwatch.StartNew();
             _logger = logger;
             _operationName = operationName;
+            _logLevel = logLevel;
         }
 
         public void Dispose()
         {
-            _logger.LogInformation($"{_operationName} completed in {_sw.ElapsedMilliseconds} ms");
             _sw.Stop();
+            if (_logger.IsEnabled(_logLevel))
+            {
+                _logger.Log(_logLevel, $"{_operationName} completed in {_sw.ElapsedMilliseconds} ms");
+            }
         }
     }
 }
