ce61dd7f9d1d0a86122442902e9a6f31a8b48620
Xavier Fischer <xavier@fischer.ninja>
2020-07-01 23:31:07 +0200

    added normal

--- DEM.Net.Core/Model/GeoPointRays.cs
+++ DEM.Net.Core/Model/GeoPointRays.cs
@@ -8,10 +8,12 @@ namespace DEM.Net.Core
     public class GeoPointRays : GeoPoint
     {
         public List<Vector3> Rays { get; set; } = new List<Vector3>();
+        public Vector3 Normal { get; set; }
 
-        public GeoPointRays(double latitude, double longitude, double? altitude, float left, float right, float up, float down)
+        public GeoPointRays(double latitude, double longitude, double? altitude, Vector3 normal, float left, float right, float up, float down)
             : base(latitude, longitude, altitude)
         {
+            this.Normal = normal;
             this.Rays.Add(Vector3.UnitX * left * -1F);
             this.Rays.Add(Vector3.UnitX * right * 1F);
             this.Rays.Add(Vector3.UnitZ * down * -1F);
